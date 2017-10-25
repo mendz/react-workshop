4601c0d57f1c9404122a065f74639c352685e3fa
Amit Zur <sendwithchibo@gmail.com>
2017-10-26 01:51:50 +0300

    unread state

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,21 @@ import SmurfItem from './SmurfItem';
 export default class App extends Component {
 
 	state = {
-		activeItem: null
+		activeItem: null,
+		data: this.props.data
 	};
 
 	onItemClick = (item) => {
+		const newData = [...this.state.data];
+		newData.splice(newData.indexOf(item), 1, Object.assign(item, {isRead: true}));
 		this.setState({
-			activeItem: item
+			activeItem: item,
+			data: newData
 		});
 	};
 
 	render() {
-		const { data } = this.props;
+		const { data } = this.state;
 
 		const content = this.state.activeItem ?
 			<Smurf {...this.state.activeItem} /> :
@@ -23,7 +27,7 @@ export default class App extends Component {
 
 		return (
 			<div className="h-100 bg-white row no-gutters">
-				<div className="col-auto br p-3 overflow-auto">
+				<div className="col-auto br overflow-auto">
 					{data.map(smurf => <SmurfItem key={smurf.name} item={smurf} onClick={this.onItemClick}/>)}
 				</div>
 				<div className="col d-flex align-items-center justify-content-center">
--- src/components/SmurfItem.js
+++ src/components/SmurfItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import cn from 'classnames';
 
-const SmurfItem = ({ item, onClick}) => (
-  <div className="d-flex align-items-center" style={{cursor:'pointer'}} onClick={() => onClick(item)}>
+const SmurfItem = ({ item, onClick }) => (
+  <div className={cn("smurf-item d-flex align-items-center bb px-3 py-2", { unread: !item.isRead })} style={{cursor:'pointer'}} onClick={() => onClick(item)}>
 		<img src={item.src} className="rounded-circle mr-3" width="75" height="75"/>
 		<div>{item.name}</div>
   </div>
