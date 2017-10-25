7ca9c35ce8c1957019c3cab1033ce67ec65091da
Amit Zur <sendwithchibo@gmail.com>
2017-10-25 23:49:34 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const { resolve } = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	devtool: 'source-map',
+	entry: [
+		'./index'
+	],
+	output: {
+		path: __dirname,
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	context: resolve(__dirname, 'src'),
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				options: {
+					presets: [["env", { modules: false }], "react"],
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: 'sass-loader'
+			}
+		]
+	},
+	plugins: [
+		new HTMLWebpackPlugin()
+	]
+};
