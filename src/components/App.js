4cedc85cb6bd24513be4c8bcb4eb1c9de59235e2
Amit Zur <sendwithchibo@gmail.com>
2017-10-26 01:57:44 +0300

    reactstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Smurf from './Smurf';
 import SmurfItem from './SmurfItem';
+import { Row, Col } from 'reactstrap';
 
 export default class App extends Component {
 
@@ -26,16 +27,16 @@ export default class App extends Component {
 			<div className="text-center text-muted">Choose a smurf on the right...</div>;
 
 		return (
-			<div className="h-100 bg-white row no-gutters">
-				<div className="col-auto br overflow-auto">
+			<Row className="h-100 bg-white" noGutters>
+				<Col xs="auto" className="br overflow-auto">
 					{data.map(smurf => <SmurfItem key={smurf.name} item={smurf} onClick={this.onItemClick}/>)}
-				</div>
-				<div className="col d-flex align-items-center justify-content-center">
+				</Col>
+				<Col className="d-flex align-items-center justify-content-center">
 					<div className="w-50">
 						{content}
 					</div>
-				</div>
-			</div>
+				</Col>
+			</Row>
 		);
 	}
 }
