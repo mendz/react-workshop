357af4fa725dad25bda2139d1dc4d0a8fa702708
Amit Zur <sendwithchibo@gmail.com>
2017-10-28 23:15:59 +0300

    resize list with drag

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ import Smurf from './Smurf';
 import { Row, Col } from 'reactstrap';
 import SmurfList from "./SmurfList";
 
+let startX;
+let startWidth;
+
 export default class App extends Component {
 
 	state = {
 		activeItem: null,
 		data: this.props.data,
-		selectedItems: []
+		selectedItems: [],
+		width: 300
 	};
 
 	onItemClick = (item) => {
@@ -42,24 +46,43 @@ export default class App extends Component {
 		});
 	};
 
-	render() {
-		const { data } = this.state;
+	onDragStart = (e) => {
+		startX = e.pageX;
+		startWidth = this.state.width;
+		document.addEventListener("mousemove", this.onDragMove);
+		document.addEventListener("mouseup", this.onDragEnd);
+	};
 
-		const content = this.state.activeItem ?
-			<Smurf {...this.state.activeItem} /> :
+	onDragMove = (e) => {
+		this.setState({
+			width: startWidth + e.pageX - startX
+		});
+	};
+
+	onDragEnd = (e) => {
+		document.removeEventListener("mousemove", this.onDragMove);
+		document.removeEventListener("mouseup", this.onDragEnd);
+	};
+
+	render() {
+		const { data, width, activeItem, selectedItems } = this.state;
+
+		const content = activeItem ?
+			<Smurf {...activeItem} /> :
 			<div className="text-center text-muted">Choose a smurf on the right...</div>;
 
 		return (
 			<Row className="h-100 bg-white" noGutters>
-				<Col xs="auto" className="br overflow-auto">
+				<Col className="br overflow-auto" style={{ flex: `0 0 ${width}px` }}>
 					<SmurfList
 						items={data}
 						onItemClick={this.onItemClick}
 						onSelectItem={this.onSelectItem}
 						resetSelection={this.resetSelection}
 						onDelete={this.onDelete}
-						canDelete={this.state.selectedItems.length > 0}/>
+						canDelete={selectedItems.length > 0}/>
 				</Col>
+				<Col style={{flex: '0 0 5px', cursor: 'move'}} onMouseDown={this.onDragStart}/>
 				<Col className="d-flex align-items-center justify-content-center">
 					<div className="w-50">
 						{content}
