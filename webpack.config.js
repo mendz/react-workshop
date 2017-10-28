04729e1cc7a2dbbcd1e4003a8a84f651c47d842b
Amit Zur <sendwithchibo@gmail.com>
2017-10-29 01:02:38 +0300

    rest and spread

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
 					presets: [["env", { modules: false }], "react"],
 					plugins: [
 						"transform-class-properties",
+						"transform-object-rest-spread",
 						"react-hot-loader/babel"
 					]
 				}
